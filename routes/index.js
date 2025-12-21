11da58688d804ee63c43726c4fc8c4760e4d9b95
Jesse Geelissen <jesse.geelissen@katholiekonderwijs.vlaanderen>
2025-12-21 12:07:46 +0000

    converted /resources to a html page

--- routes/index.js
+++ routes/index.js
@@ -8,68 +8,7 @@ router.get('/', function(req, res, next) {
 
 /* GET resources page - show available API routes */
 router.get('/resources', function(req, res, next) {
-  const resources = {
-    "organisations": {
-      "href": "/organisations",
-      "description": "Schools, boards, and umbrellas",
-      "methods": {
-        "GET": {
-          "description": "Get all organisations (supports ?type=school|board|umbrella&limit=N&offset=N)"
-        },
-        "POST": {
-          "description": "Create a new organisation"
-        }
-      }
-    },
-    "organisations/new": {
-      "href": "/organisations/new",
-      "description": "Form to create a new organisation"
-    },
-    "organisations/:uuid": {
-      "href": "/organisations/:uuid",
-      "description": "Individual organisation resource",
-      "methods": {
-        "GET": {
-          "description": "Get a specific organisation by UUID"
-        },
-        "PUT": {
-          "description": "Update an organisation by UUID"
-        },
-        "DELETE": {
-          "description": "Delete an organisation by UUID"
-        }
-      }
-    },
-    "courses": {
-      "href": "/courses",
-      "description": "Courses",
-      "methods": {
-        "GET": {
-          "description": "Get all courses (supports ?year=N)"
-        },
-        "POST": {
-          "description": "Create a new course"
-        }
-      }
-    },
-    "courses/:uuid": {
-      "href": "/courses/:uuid",
-      "description": "Individual course resource",
-      "methods": {
-        "GET": {
-          "description": "Get a specific course by UUID"
-        },
-        "PUT": {
-          "description": "Update a course by UUID"
-        },
-        "DELETE": {
-          "description": "Delete a course by UUID"
-        }
-      }
-    }
-  };
-
-  res.json(resources);
+  res.render('resources');
 });
 
 module.exports = router;
